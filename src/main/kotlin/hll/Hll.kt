2835a7a2650ebf662212cf901814aa3fa78b4017
Yan <songyan@iu.edu>
2020-07-02 16:12:15 -0400

    hll without hash and constructor

--- src/main/kotlin/hll/Hll.kt
+++ src/main/kotlin/hll/Hll.kt
@@ -1,13 +1,34 @@
 package hll
 
+import java.security.MessageDigest
+import kotlin.math.max
 import kotlin.math.pow
 
 class HyperLL<T> (val maxSize: Int) {
 
     // Private list / vector of size maxSize
-    private val counters = mutableListOf<Int>()
+    private val counters = mutableListOf<Int>(maxSize)
+    // ? initialize with size and initial value 0
+
+    private val mdIndex = MessageDigest.getInstance("SHA-256")
+    private val mdZeros = MessageDigest.getInstance("SHA-256")
+
+    private fun getIndex(bytes: ByteArray) : Int {
+        val index = mdIndex.digest(bytes)
+        return 0
+    }
+
+    private fun getLeadingZeros(bytes: ByteArray) : Int {
+        val zeros = mdZeros.digest(bytes)
+        return 0
+    }
 
     fun update(key: T) {
+        val bytes = key.hashCode().toString().toByteArray()
+        val index = getIndex(bytes)
+        val zeros = getLeadingZeros(bytes)
+
+        if (zeros > counters[index]) counters[index] = zeros
     }
 
     fun query():Double {
@@ -24,7 +45,12 @@ class HyperLL<T> (val maxSize: Int) {
 
     fun merge(summary: HyperLL<T>) {
         assert(maxSize == summary.maxSize) { "Unable to apply merge, the sizes are not equal $maxSize != ${summary.maxSize}" }
+        // ?check hash function
+
         // loop vector and pick maximum
+        summary.counters.forEachIndexed { index, element ->
+            counters[index] = max(counters[index], element)
+        }
     }
 }
 
