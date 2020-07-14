0b77efd92ce9d7c61d09d636b5027c6230bcdf77
Yan <songyan@iu.edu>
2020-07-14 18:18:10 -0400

    mg spacesaving

---
+++ src/main/kotlin/spacesaving/SpaceSaving.kt
@@ -0,0 +1,59 @@
+package spacesaving
+
+import misc.Counter
+
+class SpaceSaving<T> (val maxSize: Int) {
+    private var counters = DoubleArray(maxSize)
+    private val keys = mutableListOf<T>()
+    private var pointer = 0
+
+    init {
+        counters = DoubleArray(maxSize)
+        for (i in 0 until maxSize) {
+            counters[i] = 0.0
+        }
+    }
+
+    fun update(key: T, weight: Double = 1.0) {
+        if (pointer == 0) {
+            counters[0] = weight
+            keys.add(key)
+        }
+
+        for (i in 0 until pointer) {
+            if (key == keys[pointer]) {
+                counters[i] += weight
+                return
+            }
+        }
+
+        if (pointer < maxSize) {
+            keys.add(key)
+            counters[pointer] = weight
+            pointer += 1
+            return
+        }
+
+        purge(key, weight)
+    }
+
+    private fun purge(key: T, weight: Double) {
+        var minimum = counters[0]
+        var index = 0
+        for (i in 0 until maxSize) {
+            if (counters[i] < minimum) {
+                minimum = counters[i]
+                index = i
+            }
+        }
+        counters[index] += weight
+        keys[index] = key
+    }
+
+    fun merge(summary: SpaceSaving<T>) {
+        assert(maxSize == summary.maxSize) { "Unable to apply merge, the sizes are not equal $maxSize != ${summary.maxSize}" }
+        for (i in 0 until maxSize) {
+            update(summary.keys[i], summary.counters[i])
+        }
+    }
+}
