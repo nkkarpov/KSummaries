1c5dc02c9cf10b7810c5a0c50afd37005023a8b5
Yan <songyan@iu.edu>
2020-07-28 13:40:41 -0400

    l0 sampling init

---
+++ src/main/kotlin/l0sampling/L0Sampling.kt
@@ -0,0 +1,15 @@
+package misc.l0sampling
+
+class L0Sampling<T> {
+
+    fun update(key: T) {
+        return
+    }
+
+    fun query(): T? {
+        return null
+    }
+
+    fun merge(summary: L0Sampling<T>) {
+    }
+}
