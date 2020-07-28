fa541c6585e5e52878509d4809976cbeba31921a
Yan <songyan@iu.edu>
2020-07-28 13:36:50 -0400

    ams sketch init

---
+++ src/main/kotlin/amssketch/AmsSketch.kt
@@ -0,0 +1,16 @@
+package misc.amssketch
+
+class AmsSketch<T> (val d: Int, val t: Int)  {
+
+    fun update (key: T, weight: Double) {
+        return
+    }
+
+    fun query(key: T): Double {
+        return 0.0
+    }
+
+    fun merge (summary: AmsSketch<T>) {
+        return
+    }
+}
