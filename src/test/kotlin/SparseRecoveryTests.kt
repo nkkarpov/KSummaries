50a006a265e00a691bd3516b2c61eaa6a7460cea
Yan <songyan@iu.edu>
2020-08-04 16:10:14 -0400

    recovery merge check

--- src/test/kotlin/SparseRecoveryTests.kt
+++ src/test/kotlin/SparseRecoveryTests.kt
@@ -3,6 +3,7 @@ package frequencies.tests
 import org.junit.Test
 import sparserecovery.SparseRecovery
 import kotlin.random.Random
+import kotlin.test.assertFails
 
 class SparseRecoveryTests {
     @Test
@@ -14,5 +15,19 @@ class SparseRecoveryTests {
         }
         sketch.query()
         sketch.query()
+    }
+
+    @Test
+    fun testMergeFail() {
+        val weight = 0.5
+        val a = SparseRecovery<Int>(100, 5, 20, 3)
+        val b = SparseRecovery<Int>(100, 5)
+        for (i in 0 until 10000) {
+            val item = Random.nextInt()
+            a.update(item, weight)
+            b.update(item)
+        }
+        assertFails("Invalid merge") { a.merge(b) }
+        assertFails("Invalid merge") { b.merge(a) }
     }
 }
