90c08f6c54a7d420e5cbfc4aedb88c5d27a60405
Yan <songyan@iu.edu>
2020-08-04 23:06:06 -0400

    refined test

--- src/test/kotlin/SparseRecoveryTests.kt
+++ src/test/kotlin/SparseRecoveryTests.kt
@@ -2,32 +2,35 @@ package frequencies.tests
 
 import org.junit.Test
 import sparserecovery.SparseRecovery
+import kotlin.math.absoluteValue
 import kotlin.random.Random
 import kotlin.test.assertFails
 
 class SparseRecoveryTests {
     @Test
     fun testInit() {
-        val weight = 1
-        val sketch = SparseRecovery<Int>(100, 5, 20, 3)
-        for (i in 0 until 10000) {
-            sketch.update(Random.nextInt(), weight)
+        val weight = 0.1
+        val sketch = SparseRecovery<Int>(100, 7, 100, 5)
+        for (i in 0 until 20) {
+            sketch.update(i.rem(20)+1, weight)
         }
-        sketch.query()
         sketch.query()
     }
 
     @Test
     fun testMergeFail() {
-        val weight = 1
+        val weight = 0.1
         val a = SparseRecovery<Int>(100, 5, 20, 3)
         val b = SparseRecovery<Int>(100, 5)
+        val c = SparseRecovery<Int>(100, 5, 20, 3)
         for (i in 0 until 10000) {
-            val item = Random.nextInt()
+            val item = Random.nextInt().absoluteValue.rem(20)
             a.update(item, weight)
             b.update(item)
+            c.update(item)
         }
         assertFails("Invalid merge") { a.merge(b) }
         assertFails("Invalid merge") { b.merge(a) }
+        assertFails("Invalid merge") { a.merge(c) }
     }
 }
