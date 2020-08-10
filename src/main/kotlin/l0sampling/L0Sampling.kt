54e6f21e82d0a444085682ee0880bd9951dff40c
Yan <songyan@iu.edu>
2020-08-09 21:45:37 -0400

    l0 sampling constructor and merge

--- src/main/kotlin/l0sampling/L0Sampling.kt
+++ src/main/kotlin/l0sampling/L0Sampling.kt
@@ -1,6 +1,27 @@
 package l0sampling
 
+import sparserecovery.SparseRecovery
+import java.security.MessageDigest
+import kotlin.random.Random
+
 class L0Sampling<T> {
+
+    // number of sparse recovery
+    var m: Int
+    // store all sparse recovery
+    val recoveries = emptyArray<SparseRecovery<T>>().toMutableList()
+    // compute probability to decide which level to go to
+    val md = MessageDigest.getInstance("SHA-256")
+
+    // sparse recovery initialize with n and l
+    constructor(m: Int, n: Int, l: Int) {
+        this.m = m
+        for (i in 0 until m) {
+            recoveries.add(SparseRecovery(n, l))
+        }
+    }
+
+    constructor(m: Int, n: Int): this(m, n, 5)
 
     fun update(key: T) {
         return
@@ -11,5 +32,17 @@ class L0Sampling<T> {
     }
 
     fun merge(summary: L0Sampling<T>) {
+        // merge check
+        assert(m == summary.m) { "Unable to apply merge, the number of sizes are not equal $m != ${summary.m}" }
+
+        for (i in 0 until 10) {
+            val item = Random.nextInt().toString().toByteArray()
+            assert(md.digest(item) == summary.md.digest(item))
+            { "Unable to apply merge, the randomness used are different" }
+        }
+
+        for (i in 0 until m) {
+            recoveries[i].merge(summary.recoveries[i])
+        }
     }
 }
