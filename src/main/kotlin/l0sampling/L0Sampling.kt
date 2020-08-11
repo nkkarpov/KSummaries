eb7dab77811e32316da9fd42ebe441768ed89fd2
Yan <songyan@iu.edu>
2020-08-10 20:50:00 -0400

    l0sampling

--- src/main/kotlin/l0sampling/L0Sampling.kt
+++ src/main/kotlin/l0sampling/L0Sampling.kt
@@ -2,14 +2,15 @@ package l0sampling
 
 import sparserecovery.SparseRecovery
 import java.security.MessageDigest
+import kotlin.math.pow
 import kotlin.random.Random
 
-class L0Sampling<T> {
+class L0Sampling {
 
     // number of sparse recovery
     var m: Int
     // store all sparse recovery
-    val recoveries = emptyArray<SparseRecovery<T>>().toMutableList()
+    val recoveries = emptyArray<SparseRecovery<Int>>().toMutableList()
     // compute probability to decide which level to go to
     val md = MessageDigest.getInstance("SHA-256")
 
@@ -23,15 +24,35 @@ class L0Sampling<T> {
 
     constructor(m: Int, n: Int): this(m, n, 5)
 
-    fun update(key: T) {
+    fun update(key: Int, weight: Double = 1.0) {
+        val prob = 0.0
+        for (i in 0 until m) {
+            if (prob < 2.0.pow(i)) {
+                recoveries[i].update(key, weight)
+            } else {break}
+        }
         return
     }
 
-    fun query(): T? {
+    fun query(): Int? {
+        var res = emptyList<Int>()
+        var flag = false
+        for (i in 0 until m) {
+            // If succeed at level i
+            if (recoveries[i].query() != null) {
+                res = recoveries[i].query()!!
+                flag = true
+                break
+            }
+        }
+        if (flag) {
+            res.toMutableList().shuffle()
+            return res[0]
+        }
         return null
     }
 
-    fun merge(summary: L0Sampling<T>) {
+    fun merge(summary: L0Sampling) {
         // merge check
         assert(m == summary.m) { "Unable to apply merge, the number of sizes are not equal $m != ${summary.m}" }
 
