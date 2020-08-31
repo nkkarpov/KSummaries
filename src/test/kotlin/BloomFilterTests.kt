2400d87b6096383e4324058c9a7afe2516816d85
Yan <songyan@iu.edu>
2020-08-30 20:23:59 -0400

    updated tests

--- src/test/kotlin/BloomFilterTests.kt
+++ src/test/kotlin/BloomFilterTests.kt
@@ -1,6 +1,7 @@
-package frequencies.tests
+package tests
 
 import bloom.BloomFilter
+import filereader.FileReaderBuffer
 import org.junit.Test
 import kotlin.random.Random
 import kotlin.test.assertFails
@@ -27,5 +28,35 @@ class BloomFilterTests {
             b.update(item)
         }
         assertFails("Invalid merge") { a.merge(b) }
+    }
+
+    fun testFile(fileName: String, maxSize: Int, k: Int) {
+        val reader = FileReaderBuffer(fileName)
+        val bf = BloomFilter<String>(maxSize, k)
+        var word: String?
+        word = reader.next()
+        while (word != null) {
+            bf.update(word)
+            word = reader.next()
+        }
+
+        var error_rate = 0.0
+        var errors = 0
+        for (i in 1001 until 2000) {
+            val item = i.toString()
+            if (bf.query(item))
+                errors += 1
+        }
+        error_rate = errors / 1000.0
+        println(("Error rate on T40I10D100K dataset with %d slots and %d hash functions " +
+                "is %.2f %%").format(maxSize, k, error_rate*100))
+    }
+
+    @Test
+    fun testT40I10D100K() {
+        val fileName = "data/T40I10D100K.txt"
+        val maxSize = 100
+        val k = 5
+        testFile(fileName, maxSize, k)
     }
 }
