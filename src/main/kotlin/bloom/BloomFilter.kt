dc7ea16c209900542ebe8e3c32d03a45d601480a
Yan <songyan@iu.edu>
2020-09-02 21:33:18 -0400

    changed to boolean

--- src/main/kotlin/bloom/BloomFilter.kt
+++ src/main/kotlin/bloom/BloomFilter.kt
@@ -8,69 +8,49 @@ The returned results may contain false positives but no false negatives.
 
 package bloom
 
-import java.security.MessageDigest
-import kotlin.math.abs
+import saltedhash.SaltedHash
 import kotlin.math.max
-import kotlin.random.Random
 
+// k hash functions
 class BloomFilter<T> (val maxSize: Int, val k: Int) {
-    private val counters = mutableListOf<Int>()
-    private val hashes = mutableListOf<MessageDigest>()
+    private val counters = mutableListOf<Boolean>()
+    private val hashes: SaltedHash
 
     init {
         for (i in 0 until maxSize) {
-            counters.add(0)
+            counters.add(false)
         }
-        for (i in 0 until k) {
-            var hash = MessageDigest.getInstance("SHA-256")
-            hashes.add(hash)
-        }
-    }
-
-    private fun getIndex(bytes: ByteArray) : Int {
-        var result = 0
-        var shift = 0
-        for (i in 0 until 8) {
-            result = result or (bytes.get(i).toInt() shl shift)
-            shift += 8
-        }
-        return abs(result).rem(maxSize)
+        hashes = SaltedHash(k)
     }
 
     fun update(key: T) {
         val hashcode = key.hashCode()
         for (i in 0 until k) {
-            val bytes = hashes[i].digest(hashcode.toString().toByteArray())
-            val index = getIndex(bytes)
-            counters[index] = 1
+            val index = hashes.getIndex(i, hashcode.toString(), maxSize)
+            counters[index] = true
         }
     }
 
     fun query(key: T): Boolean {
         val hashcode = key.hashCode()
         for (i in 0 until k) {
-            val bytes = hashes[i].digest(hashcode.toString().toByteArray())
-            val index = getIndex(bytes)
-            if (counters[index] != 1) return false
+            val index = hashes.getIndex(i, hashcode.toString(), maxSize)
+            if (!counters[index]) return false
         }
         return true
     }
 
     fun merge(summary: BloomFilter<T>) {
-        assert(maxSize == summary.maxSize) { "Unable to apply merge, the sizes are not equal $maxSize != ${summary.maxSize}" }
-        assert(k == summary.k) { "Unable to apply merge, the number of hash functions are not equal $k != ${summary.k}" }
-
-        for (i in 0 until k) {
-            for (j in 0 until 5) {
-                val item = Random.nextInt().toString().toByteArray()
-                assert(hashes[i].digest(item) == summary.hashes[i].digest(item))
-                { "Unable to apply merge, the randomness used are different" }
-            }
-        }
+        assert(maxSize == summary.maxSize)
+        { "Unable to apply merge, the sizes are not equal $maxSize != ${summary.maxSize}" }
+        assert(k == summary.k)
+        { "Unable to apply merge, the number of hash functions are not equal $k != ${summary.k}" }
+        assert(hashes.mergeableWith(summary.hashes))
+        { "Unable to apply merge, the randomness used are different" }
 
         // loop list and pick bigger
         summary.counters.forEachIndexed { index, element ->
-            counters[index] = max(counters[index], element)
+            counters[index] = (counters[index] and element)
         }
     }
 }
