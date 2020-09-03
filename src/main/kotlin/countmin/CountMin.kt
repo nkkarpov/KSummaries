87499cf29c8197e9971a69e3369c6e69c2daa73c
Yan <songyan@iu.edu>
2020-09-02 22:52:44 -0400

    simplified code

--- src/main/kotlin/countmin/CountMin.kt
+++ src/main/kotlin/countmin/CountMin.kt
@@ -8,26 +8,38 @@ This summary only accepts positive weights.
 
 package countmin
 
-import java.security.MessageDigest
+import saltedhash.SaltedHash
 import kotlin.math.absoluteValue
 import kotlin.math.min
 import kotlin.math.pow
-import kotlin.random.Random.Default.nextInt
+import kotlin.random.Random
 
-class CountMin<T> (val d: Int, val t: Int) {
-    private var counters = Array(d, { Array(t, {0.0})})
-    private var arrA = Array(d, {0})
-    private var arrB = Array(d, {0})
+// counters as a d by t matrix
+class CountMin<T> {
+    private var counters: Array<Array<Double>>
+    private var arrA: Array<Int>
+    private var arrB: Array<Int>
     private val p = 7368787
-    private val md = MessageDigest.getInstance("SHA-256")
+    private var hash: SaltedHash
+    private var d: Int
+    private var t: Int
 
-    init {
-        // Initialize hash functions
+    constructor(d: Int, t: Int, intSeed: Int, hashSeed: Int) {
+        this.d = d
+        this.t = t
+        arrA = Array(d, {0})
+        arrB = Array(d, {0})
+        counters = Array(d, { Array(t, {0.0})})
+
+        // Initialize hash array
+        val rd = Random(intSeed)
         for (i in 0 until d) {
-            arrA[i] = nextInt(p)
-            arrB[i] = nextInt(p)
+            arrA[i] = rd.nextInt(p).absoluteValue
+            arrB[i] = rd.nextInt(p).absoluteValue
         }
+        hash = SaltedHash(1, hashSeed)
     }
+    constructor(d: Int, t: Int): this(d, t, 10, 10)
 
     fun update (key: T, weight: Double) {
         val hash = getHash(key)
@@ -42,7 +54,7 @@ class CountMin<T> (val d: Int, val t: Int) {
         val hash = getHash(key)
         for (i in 0 until d) {
             val index = getIndex(hash, i)
-             res = min(res, counters[i][index])
+            res = min(res, counters[i][index])
         }
         return res
     }
@@ -50,6 +62,7 @@ class CountMin<T> (val d: Int, val t: Int) {
     fun merge (summary: CountMin<T>) {
         assert(d == summary.d) { "Unable to apply merge, the size of rows is not equal $d != ${summary.d}" }
         assert(t == summary.t) { "Unable to apply merge, the size of cols is not equal $t != ${summary.t}" }
+        assert(hash.mergeableWith(summary.hash)) {  "Unable to apply merge, different randomness applied!" }
         for (i in 0 until d) {
             assert(arrA[i] == summary.arrA[i]) {  "Unable to apply merge, different randomness applied!" }
             assert(arrB[i] == summary.arrB[i]) {  "Unable to apply merge, different randomness applied!" }
@@ -66,14 +79,10 @@ class CountMin<T> (val d: Int, val t: Int) {
         return (arrA[index] * hash + arrB[index]).rem(p).rem(t).absoluteValue
     }
 
+    private fun getHash(key: String): Int {
+        return hash.getHash(0, key)
+    }
     private fun getHash(key: T) : Int {
-        val bytes = md.digest(key.hashCode().toString().toByteArray())
-        var result = 0
-        var shift = 0
-        for (i in 0 until 8) {
-            result = result or (bytes.get(i).toInt() shl shift)
-            shift += 8
-        }
-        return result.absoluteValue.rem(p)
+        return hash.getHash(0, key.hashCode().toString())
     }
 }
