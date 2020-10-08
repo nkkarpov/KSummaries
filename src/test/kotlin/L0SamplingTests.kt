dc5593e72e5d16abaf376e0fa7b118cb7562df67
Yan <songyan@iu.edu>
2020-10-07 20:11:31 -0400

    updated tests

--- src/test/kotlin/L0SamplingTests.kt
+++ src/test/kotlin/L0SamplingTests.kt
@@ -1,5 +1,6 @@
 package frequencies.tests
 
+import filereader.FileReaderBuffer
 import l0sampling.L0Sampling
 import org.junit.Test
 import kotlin.math.absoluteValue
@@ -20,7 +21,7 @@ class L0SamplingTests {
     @Test
     fun testMergeFail() {
         val weight = 0.1
-        val a = L0Sampling(20, 100)
+        val a = L0Sampling(20, 100, 10)
         val b = L0Sampling(20, 100)
         for (i in 0 until 10000) {
             val item = Random.nextInt().absoluteValue.rem(20)
@@ -29,5 +30,74 @@ class L0SamplingTests {
         }
         assertFails("Invalid merge") { a.merge(b) }
         assertFails("Invalid merge") { b.merge(a) }
+    }
+
+    fun testFiles(fileName1: String, fileName2: String,
+                  m: Int, n: Int, l: Int, maxSize: Int, k: Int, hashSeed: Int = 100) {
+        // Load input files
+        val reader1 = FileReaderBuffer(fileName1+".txt")
+        val reader2 = FileReaderBuffer(fileName2+".txt")
+        // Create data structures
+        val sp1 = L0Sampling(m, n, l, maxSize, k, hashSeed)
+        val sp2 = L0Sampling(m, n, l, maxSize, k, hashSeed)
+        val sp1Copy = L0Sampling(m, n, l, maxSize, k, hashSeed)
+        // Create two sets as ground truth
+        val t1 = emptySet<Int>().toMutableList()
+        val t2 = emptySet<Int>().toMutableList()
+
+        // Feed input into data structures
+        var word: String?
+        word = reader1.next()
+        while (word != null) {
+            sp1.update(word.toInt())
+            sp1Copy.update(word.toInt())
+            if (!t1.contains(word.toInt())) {
+                t1.add(word.toInt())
+            }
+            word = reader1.next()
+        }
+        word = reader2.next()
+        while (word != null) {
+            sp2.update(word.toInt())
+            if (!t2.contains(word.toInt())) {
+                t2.add(word.toInt())
+            }
+            word = reader2.next()
+        }
+
+        // Merge
+        sp1Copy.merge(sp2)
+
+        val res1 = emptyList<Int>().toMutableList()
+        val res2 = emptyList<Int>().toMutableList()
+        val res3 = emptyList<Int>().toMutableList()
+        for (i in 0 until 10) {
+            var item1 = sp1.query()
+            var item2 = sp2.query()
+            var item3 = sp1Copy.query()
+            if (item1 != null) res1.add(item1)
+            if (item2 != null) res2.add(item2)
+            if (item3 != null) res3.add(item3)
+        }
+        println(res1)
+        println(sp1.tempResults.size)
+        println(res2)
+        println(sp2.tempResults.size)
+        println(res3)
+        println(sp1Copy.tempResults.size)
+    }
+
+    @Test
+    fun testMergeT1T2() {
+        val fileNameT1 = "data/T10I4D100K"
+        val fileNameT2 = "data/T40I10D100K"
+        testFiles(fileNameT1, fileNameT2, 10, 2000, 5, 500, 5)
+    }
+
+    @Test
+    fun testMergeT2K() {
+        val fileNameT2 = "data/T40I10D100K"
+        val fileNameK = "data/kosarak"
+        testFiles(fileNameT2, fileNameK, 10, 10000, 5, 500, 5)
     }
 }
--- src/test/kotlin/SparseRecoveryTests.kt
+++ src/test/kotlin/SparseRecoveryTests.kt
@@ -1,5 +1,6 @@
 package frequencies.tests
 
+import filereader.FileReaderBuffer
 import org.junit.Test
 import sparserecovery.SparseRecovery
 import kotlin.math.absoluteValue
@@ -22,7 +23,7 @@ class SparseRecoveryTests {
         val weight = 0.1
         val a = SparseRecovery<Int>(100, 5, 20, 3)
         val b = SparseRecovery<Int>(100, 5)
-        val c = SparseRecovery<Int>(100, 5, 20, 3)
+        val c = SparseRecovery<Int>(100, 5, 20, 3, 20)
         for (i in 0 until 10000) {
             val item = Random.nextInt().absoluteValue.rem(20)
             a.update(item, weight)
@@ -32,5 +33,84 @@ class SparseRecoveryTests {
         assertFails("Invalid merge") { a.merge(b) }
         assertFails("Invalid merge") { b.merge(a) }
         assertFails("Invalid merge") { a.merge(c) }
+    }
+
+    fun testQueryResults(sr: SparseRecovery<Int>, truth: Set<Int>) {
+        val res = sr.query()
+        if (res == null) {
+            println("Cannot recovery")
+        } else if (res.size != truth.size) {
+            println("Did not recovery whole set. Recovered ${res.size} items out of ${truth.size}")
+        } else {
+            for (i in res.indices) {
+                if (!truth.contains(res[i])) {
+                    println("Recovered unrecognized item")
+                    return
+                }
+            }
+            println("Successful recovery")
+        }
+    }
+
+    fun testFiles(fileName1: String, fileName2: String,
+                  n: Int, l: Int, maxSize: Int, k: Int, hashSeed: Int = 100) {
+        // Load input files
+        val reader1 = FileReaderBuffer(fileName1+".txt")
+        val reader2 = FileReaderBuffer(fileName2+".txt")
+        // Create data structures
+        val sr1 = SparseRecovery<Int>(n, l, maxSize, k, hashSeed)
+        val sr2 = SparseRecovery<Int>(n, l, maxSize, k, hashSeed)
+        val sr1Copy = SparseRecovery<Int>(n, l, maxSize, k, hashSeed)
+        // Create two sets as ground truth
+        val t1 = emptySet<Int>().toMutableList()
+        val t2 = emptySet<Int>().toMutableList()
+        val t3 = emptySet<Int>().toMutableList()
+
+        // Feed input into data structures
+        var word: String?
+        word = reader1.next()
+        while (word != null) {
+            sr1.update(word.toInt())
+            sr1Copy.update(word.toInt())
+            if (!t1.contains(word.toInt())) {
+                t1.add(word.toInt())
+            }
+            if (!t3.contains(word.toInt())) {
+                t3.add(word.toInt())
+            }
+            word = reader1.next()
+        }
+        word = reader2.next()
+        while (word != null) {
+            sr2.update(word.toInt())
+            if (!t2.contains(word.toInt())) {
+                t2.add(word.toInt())
+            }
+            if (!t3.contains(word.toInt())) {
+                t3.add(word.toInt())
+            }
+            word = reader2.next()
+        }
+
+        // Merge
+        sr1Copy.merge(sr2)
+
+        testQueryResults(sr1, t1.toSet())
+        testQueryResults(sr2, t2.toSet())
+        testQueryResults(sr1Copy, t3.toSet())
+    }
+
+    @Test
+    fun testMergeT1T2() {
+        val fileNameT1 = "data/T10I4D100K"
+        val fileNameT2 = "data/T40I10D100K"
+        testFiles(fileNameT1, fileNameT2, 2000, 5, 500, 5)
+    }
+
+    @Test
+    fun testMergeT2K() {
+        val fileNameT2 = "data/T40I10D100K"
+        val fileNameK = "data/kosarak"
+        testFiles(fileNameT2, fileNameK, 100000, 5, 1000, 5)
     }
 }
